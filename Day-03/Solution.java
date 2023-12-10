d478bc1970d7a8cab44924c69da5677c80cbd2dc
alimajeed0 <brand.heckle-0c@icloud.com>
2023-12-10 15:42:10 +0300

    Cleaning up

--- Day-03/Solution.java
+++ Day-03/Solution.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 public class Solution {
     
-    // List to hold all part numbers 
-    public static List<String> resultList = new ArrayList<>();
+    // List to store all extracted part numbers
+    public static List<String> partNumbersList = new ArrayList<>();
 
-    // Main method
     public static void main(String[] args) {
-        
         // Input text
         String engineSchematic = 
             "467..114..\n" +
@@ -24,36 +22,36 @@ public class Solution {
             ".664.598..";
        
         // Split into lines and store them as a list of strings
-        List<String> linesList = Arrays.asList(engineSchematic.split("\n"));
+        List<String> schematicList = Arrays.asList(engineSchematic.split("\n"));
         
-        // Convert the lines into 2-D array
-        char[][] twoDimArray = convertToTwoDimArray(linesList);
+        // Convert the list to a 2D array representation
+        char[][] schematicArray = convertListToTwoDimArray(schematicList);
         
         // Find part numbers
-        findAdjacentPartNumbers(twoDimArray, linesList.get(0).length(), linesList.size());
+        findAdjacentPartNumbers(schematicArray, schematicList.get(0).length(), schematicList.size());
 
-        // Calculate the sum of part numbers
+        // Calculate the sum of all part numbers
         int sum = 0;
-        for (String s : resultList) {
+        for (String s : partNumbersList) {
             sum += Integer.parseInt(s);
         }
         System.out.println("The sum of all part numbers = "+sum);
     }
 
-    // Method to convert the lines list to 2-D array
-    private static char[][] convertToTwoDimArray(List<String> linesList) { 
-        char[][] twoDimArray = new char[linesList.get(0).length()][linesList.size()];
-        for (int i = 0; i < linesList.size(); i++) { 
-            for (int j = 0; j < linesList.get(i).length(); j++) {
-                twoDimArray[i][j] = linesList.get(i).charAt(j);
+    // Method to convert the list of lines into a 2D character array
+    private static char[][] convertListToTwoDimArray(List<String> list) { 
+        char[][] twoDimArray = new char[list.get(0).length()][list.size()];
+        for (int i = 0; i < list.size(); i++) { 
+            for (int j = 0; j < list.get(i).length(); j++) {
+                twoDimArray[i][j] = list.get(i).charAt(j);
             }
         }
         return twoDimArray;
     }
 
+    // Method to identify and extract all part numbers within the schematic array
     private static void findAdjacentPartNumbers(char[][] twoDimArray, int cols, int rows) {
-        // StringBuilder to construct a part number
-        StringBuilder partNumber = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
         // Flag to indicate if the current digit is part of a valid part number
         boolean isPartNumber = false;
 
@@ -61,31 +59,32 @@ public class Solution {
             for (int j = 0; j < cols; j++) {
                 if (Character.isDigit(twoDimArray[i][j])) {
                     // Append the character to the current part number
-                    partNumber.append(twoDimArray[i][j]);
-                    if (isDigitAdjacentToSymbol(twoDimArray,i,j)) {
+                    builder.append(twoDimArray[i][j]);
+                    if (hasAdjacentSymbol(twoDimArray,i,j)) {
                         isPartNumber = true;
                     }
                 } else {
                     if (isPartNumber) {
                         // If a valid number is complete, add it to the result list
-                        resultList.add(partNumber.toString());
+                        partNumbersList.add(builder.toString());
                     }
                     isPartNumber = false;
                     // Clears the StringBuilder
-                    partNumber = new StringBuilder(); 
+                    builder = new StringBuilder(); 
                 }
             }
         }
     }
 
-    private static boolean isDigitAdjacentToSymbol(char[][] twoDimArray, int x, int y) {
+    // Checks if a digit has a symbol adjacent to it
+    private static boolean hasAdjacentSymbol(char[][] twoDimArray, int x, int y) {
         // Check surrounding characters
-        for(int i = -1; i <= 1; i++) {
-            for(int j = -1; j <= 1; j++) {
+        for(int dx = -1; dx <= 1; dx++) {
+            for(int dy = -1; dy <= 1; dy++) {
                 // Skip invalid adjacent cells 
-                if (!isAdjacentCellValid(x, y, i, j, twoDimArray)) 
+                if (!isCellWithinBounds(x, y, dx, dy, twoDimArray)) 
                     continue;
-                if (isSymbol(twoDimArray[x + i][y + j])) {
+                if (isSymbol(twoDimArray[x + dx][y + dy])) {
                     // If a symbol is found in any of the adjacent cells
                     return true;
                 }
@@ -96,10 +95,11 @@ public class Solution {
     }
 
     // Method checks if the given coordinates represent a valid adjacent cell within the array
-    private static boolean isAdjacentCellValid(int x, int y, int dx, int dy, char[][] data) {
+    private static boolean isCellWithinBounds(int x, int y, int dx, int dy, char[][] data) {
         return ((x + dx >= 0) && (x + dx < data.length)  && (y + dy >= 0) && (y + dy < data[x + dx].length) && (!(dx == 0 && dy == 0)));
     }
     
+    // Checks if a character is not alphanumeric or dot
     private static boolean isSymbol(char c) {
         return Character.toString(c).matches("[^a-zA-Z0-9.]");
     }
